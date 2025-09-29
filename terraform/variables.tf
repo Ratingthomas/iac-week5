93c5657027ff93d1eb71028e642dde4a4901e0ca
Thomas <554190@student.saxion.nl>
2025-09-29 10:15:51 +0000

    added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "esxi_hostname" {
+  default = "192.168.100.2"
+}
+
+variable "esxi_hostport" {
+  default = "22"
+}
+
+variable "esxi_hostssl" {
+  default = "443"
+}
+
+variable "esxi_username" {
+  default = "root"
+}
+
+variable "esxi_password" { # Unspecified will prompt
+    default = "Welkom01!"
+}
+
+variable "ovf_file" {
+  default = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
+}
